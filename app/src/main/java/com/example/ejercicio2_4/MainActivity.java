c34a3cc068800cf9769106395dc6f22794a679af
Luis <luigipena23@gmail.com>
2022-03-24 01:33:13 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio2_4/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.ejercicio2_4;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ejercicio2_4.db.DbSignaturess;
+
+public class MainActivity extends AppCompatActivity {
+    Lienzo lienzo_firma;
+    EditText txt_descripcion;
+    Button btn_verfirmas;
+    Button btn_salvarfirma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_salvarfirma = findViewById(R.id.salvar);
+        btn_salvarfirma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lienzo_firma = findViewById(R.id.imgfirma);
+                txt_descripcion = findViewById(R.id.descripcion);
+
+                String descripcion = txt_descripcion.getText().toString();
+                lienzo_firma.buildDrawingCache();
+                Bitmap firma = lienzo_firma.getDrawingCache();
+
+                if(descripcion.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Ingrese una descripción", Toast.LENGTH_SHORT).show();
+                } else {
+                    DbSignaturess dbFirmas = new DbSignaturess(MainActivity.this);
+                    long id = dbFirmas.insertarFirma(firma, descripcion);
+                    if (id>0){
+                        Toast.makeText(getApplicationContext(),"Registro Guardado", Toast.LENGTH_SHORT).show();
+                        limpiar();
+                        finish();
+                        startActivity(getIntent()); //Recargar Activity
+                    }
+                    else{Toast.makeText(getApplicationContext(),"No se guardó el Registro", Toast.LENGTH_SHORT).show();;}
+                }
+            }
+        });
+
+        btn_verfirmas = findViewById(R.id.verfirmas);
+        btn_verfirmas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), FirmasCards.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void limpiar(){
+        lienzo_firma.NuevoDibujo();
+        txt_descripcion.setText("");
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/ejercicio2_4/db/entidades/Signaturess.java
@@ -0,0 +1,40 @@
+package com.example.ejercicio2_4.db.entidades;
+
+public class Signaturess {
+    private int id;
+    private byte[] firma;
+    String descripcion;
+
+    public Signaturess(){
+    }
+
+    public Signaturess(int id, byte[] firma, String descripcion) {
+        this.id = id;
+        this.firma = firma;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getFirma() {
+        return firma;
+    }
+
+    public void setFirma(byte[] firma) {
+        this.firma = firma;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
